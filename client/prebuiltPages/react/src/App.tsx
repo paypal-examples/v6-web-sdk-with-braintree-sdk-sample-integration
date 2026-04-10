1ed1b990b9d791da568242a4e86ff4dc404c9f08
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2026-04-10 09:30:14 -0500

    implement client token usage

--- client/prebuiltPages/react/src/App.tsx
+++ client/prebuiltPages/react/src/App.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import { BraintreePayPalProvider } from "@paypal/react-paypal-js/sdk-v6";
 import type { BraintreeV6Namespace } from "@paypal/react-paypal-js/sdk-v6";
+import { getBraintreeBrowserSafeClientToken } from "../utils";
 
 declare global {
   interface Window {
@@ -8,11 +10,30 @@ declare global {
 }
 
 function App() {
+  const [clientToken, setClientToken] = useState<string>("");
+
+  useEffect(() => {
+    async function fetchClientToken() {
+      const braintreeClientToken = await getBraintreeBrowserSafeClientToken();
+
+      setClientToken(braintreeClientToken);
+    }
+
+    fetchClientToken();
+  }, []);
+
+  if (!clientToken) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>React Prebuilt Page</h1>
       <p>Edit src/App.tsx and save to see changes.</p>
-      <BraintreePayPalProvider namespace={window.braintree} braintreeClientToken="">
+      <BraintreePayPalProvider
+        namespace={window.braintree}
+        braintreeClientToken={clientToken}
+      >
         some text here
       </BraintreePayPalProvider>
     </div>
