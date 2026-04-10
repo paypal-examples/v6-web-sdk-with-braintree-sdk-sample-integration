f94b06783681b5e1f51f56f25ae329bf91cc3077
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2026-04-10 09:30:14 -0500

    implement client token usage

---
+++ client/prebuiltPages/react/utils.ts
@@ -0,0 +1,14 @@
+export async function getBraintreeBrowserSafeClientToken() {
+  const response = await fetch(
+    "/braintree-api/auth/browser-safe-client-token",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const { clientToken } = await response.json();
+
+  return clientToken;
+}
