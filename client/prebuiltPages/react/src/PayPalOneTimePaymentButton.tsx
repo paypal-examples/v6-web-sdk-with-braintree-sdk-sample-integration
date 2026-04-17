c578922371a8ed6394af5b7db31bcaacd7563a11
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2026-04-17 14:51:22 -0500

    add paypal one time payment hook usage

---
+++ client/prebuiltPages/react/src/PayPalOneTimePaymentButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useBraintreePayPalOneTimePaymentSession } from "@paypal/react-paypal-js/sdk-v6";
+import type { UseBraintreePayPalOneTimePaymentSessionProps } from "@paypal/react-paypal-js/sdk-v6";
+
+export const PayPalOneTimePaymentButton: React.FC<UseBraintreePayPalOneTimePaymentSessionProps> = (props) => {
+  const { isPending, handleClick } =
+    useBraintreePayPalOneTimePaymentSession(props);
+
+  return (
+    <paypal-button onClick={() => handleClick()} disabled={isPending}></paypal-button>
+  );
+}
